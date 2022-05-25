e7e8c7524dc791da3628a990e71d67533e6b8e56
GREYLABS <61804496+aggonzal-ibm@users.noreply.github.com>
2022-05-25 15:08:39 -0500

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 
 const db = mysql.createConnection({
-  host: 'bqks27iw4wrbzemn73ot-mysql.services.clever-cloud.com',
-  user:'u6ntptrtg3vlx7vu',
-  password:'QYyvRel4Z8ilROO2TKNI',
-  database:'bqks27iw4wrbzemn73ot'  
+  host: 'mysql://48jfgy6uwofv:pscale_pw_BsBxLasqk3kNH4xsnbz-S_wOmmPUDFxls-I7Dvsne44@vi8o089qy461.us-east-3.psdb.cloud/montydb?ssl={"rejectUnauthorized":true}',
+  user:'48jfgy6uwofv',
+  password:'pscale_pw_BsBxLasqk3kNH4xsnbz-S_wOmmPUDFxls-I7Dvsne44',
+  database:'montydb'  
 });
 
 db.connect(function(err){
